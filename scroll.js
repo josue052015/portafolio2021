cb98af2a5fbb53c826e052567df068ca6465f637
josue052015 <76499684+josue052015@users.noreply.github.com>
2021-02-15 04:11:06 +0100

    Initial commit

---
+++ scroll.js
@@ -0,0 +1,137 @@
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado2');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover2 1s'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado3');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover2 1s'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado4');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
+
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado5');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover3 1s'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado6');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
+
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado0');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover4 1s'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+     
+    }
+})
+
+window.addEventListener('scroll',function(){
+    let animacion = document.getElementById('animado11');
+    let posicion = animacion.getBoundingClientRect().top
+    console.log(posicion);
+    let tamañopantalla = window.innerHeight/1.3;
+    if(posicion < tamañopantalla){
+        animacion.style.animation = 'mover4 1s'
+        animacion.style.opacity = 100
+    }
+    
+    let tamañopantalla2 = window.innerHeight - 75;
+    if(posicion > tamañopantalla2){
+        animacion.style.animation = null
+        animacion.style.opacity = 0
+    }
+})
